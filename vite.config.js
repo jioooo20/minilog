8eb18945a1b05ab2d32d27be0328f859b67d044a
jioooo20 <giojio825@gmail.com>
2026-05-24 00:53:26 +0700

    dev

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
         port: 5173,
         strictPort: true,
         hmr: {
-            host: '192.168.1.9',
+            host: true,
         }
     },
     plugins: [
